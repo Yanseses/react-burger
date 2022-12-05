a51ad1cf30ea3650593bccf46bb9b89b4d4cca65
Yanseses <s0dom546@gmail.com>
2022-12-05 15:25:56 +0300

    newNameApp

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App1/App1';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
